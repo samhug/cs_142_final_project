0efbf124f1fcca418af52a2b612acb479d4f23c0
jataca <caspy200@hotmail.com>
2012-12-09 16:46:39 -0800

    Update src/PlethoraOfPong.java
    Added another JOptionpane and so people can choose the number of balls they want, however, if there are more balls than one it does some screwy stuff when they hit the goal lines

--- src/PlethoraOfPong.java
+++ src/PlethoraOfPong.java
@@ -13,260 +13,271 @@ import engine.CollisionEngine;
 import engine.CollisionEngine.CollisionEvent;
 import engine.Engine;
 
-public class PlethoraOfPong extends Engine {
-	
-	// The name of the game.
-	private static final String NAME = "Plethora Of Pong";
-	
-	// Predefined list of keys to assign to players.
-	private static final int[][] PLAYER_KEYS = {
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			// { KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-			{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
-	/*
-	 * { KeyEvent.VK_Q, KeyEvent.VK_A }, { KeyEvent.VK_W, KeyEvent.VK_S }, {
-	 * 
-	 * KeyEvent.VK_E, KeyEvent.VK_D }, { KeyEvent.VK_R, KeyEvent.VK_F }, {
-	 * KeyEvent.VK_T, KeyEvent.VK_G },
-	 */
+public class FinalProject extends Engine {
 
-	};
-	
-	/**
-	 * The number of seconds to delay the game in between points. 
-	 */
-	private static final int COUNTDOWN_DELAY = 5;
-	
-	/**
-	 * The number of points that causes a player to lose. 
-	 */
-	private static final int LOSING_SCORE = 3;
+// The name of the game.
+private static final String NAME = "Plethora Of Pong";
 
-	/**
-	 * The point on the screen that the ball should start at.
-	 */
-	private static final Point2D BALL_START_POSITION = new Point2D.Double(0, 0);
-	
-	/**
-	 * The number of players in the game.
-	 */
-	private final int nPlayers;
+// Predefined list of keys to assign to players.
+private static final int[][] PLAYER_KEYS = {
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+// { KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT },
+{ KeyEvent.VK_Q, KeyEvent.VK_A },
+{ KeyEvent.VK_Y, KeyEvent.VK_H },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+{ KeyEvent.VK_UP, KeyEvent.VK_DOWN },
+/*
+* { KeyEvent.VK_Q, KeyEvent.VK_A }, { KeyEvent.VK_W, KeyEvent.VK_S }, {
+*
+* KeyEvent.VK_E, KeyEvent.VK_D }, { KeyEvent.VK_R, KeyEvent.VK_F }, {
+* KeyEvent.VK_T, KeyEvent.VK_G },
+*/
 
-	public Paddle[] paddles;
-	public GoalLine[] goalLines;
+};
 
-	private Ball ball;
-	
-	// This timer is used to provide a delay at the beginning of the game.
-	private Timer countdownTimer;
-	private int countdownValue;
-	
-	private boolean isGameOver = false;
-	private int loserPlayer;
+// Delays the start of the game with a 5 second count down.
+private static final int COUNTDOWN_DELAY = 3;
 
-	/**
-	 * Constructor
-	 */
-	public PlethoraOfPong(int nPlayers) {
-		// Initialize the game engine with a name.
-		super(NAME);
+// The number of points a player gets before he loses.
+private static final int LOSING_SCORE = 3;
 
-		this.nPlayers = nPlayers;
+// The point on the screen that the Pong ball should start at.
+private static final Point2D BALL_START_POSITION = new Point2D.Double(0, 0); // Center
+// of
+// screen
 
-		System.out.println("Starting a " + nPlayers + " player game.");
+// The number of players to use.
+private final int nPlayers;
+private final int nBalls;
 
-		// Initialize the game
-		initialize();
-	}
+// private int nBalls
 
-	/**
-	 * Initialize all game objects.
-	 */
-	public void initialize() {
-		super.initialize();
+public Paddle[] paddles;
+public GoalLine[] goalLines;
 
-		ball = new Ball(BALL_START_POSITION);
+// public Ball[] balls;
 
-		addObject(ball);
+private Ball ball;
+public Ball moreballs;
 
-		initializePlayers();
-	}
-	
-	public void start() {
-		super.start();
-		
-		reset();
-	}
+// This timer is used to provide a delay at the beginning of the game.
+private Timer countdownTimer;
+private int countdownValue;
 
-	protected void onUpdate(long tick) {}
+private boolean isGameOver = false;
+private int loserPlayer;
 
-	protected void onRender(Graphics2D g) {
-		
-		// If the count down is in-progress display the countdown value. 
-		if (countdownValue > 0) {
-			g.setColor(Color.GREEN);
-			g.scale(1, -1);
-			g.translate(-3, 3);
-			g.drawString(Integer.toString(countdownValue), 0, 0);
-		}
-		
-		if (isGameOver) {
-			g.setColor(Color.RED);
-			g.scale(0.15, -0.15);
-			g.translate(-45, 20);
-			g.drawString("Player " + loserPlayer + " Loses!", 0, 0);
-		}
-	}
-	
-	public void onPlayerLose(GoalLine goal) {		
-		ball.reset();
-		goal.incrementScore();
-		
-		if (goal.getScore() == LOSING_SCORE) {
-			soundEngine.playSound("death.wav");
-			System.out.println("Player " + goal.playerNum + " loses!!");
-			isGameOver = true;
-			loserPlayer = goal.playerNum;
-		} else {
-			soundEngine.playSound("oops.wav");
-			goal.updateColor();
-			reset();
-		}
-	}
-	
-	/**
-	 * Splits the circumference of a circle into `2*N_PLAYERS - 1` points to act
-	 * as the boundaries for `N_PLAYERS` player paddles, arranged in a polygon
-	 * with `N_PLAYERS` sides.
-	 */
-	private void initializePlayers() {
+/**
+* Constructor
+*/
+public FinalProject(int nPlayers, int nBalls) {
+// Initialize the game engine with a name.
+super(NAME);
 
-		final double angleIncrement = 2 * Math.PI / nPlayers;
+this.nPlayers = nPlayers;
+this.nBalls = nBalls; 
 
-		paddles = new Paddle[nPlayers];
-		goalLines = new GoalLine[nPlayers];
+System.out.println("Starting a " + nPlayers + " player game.");
 
-		Point2D a, b;
+// Initialize the game
+initialize();
+}
 
-		for (int i = 0; i < nPlayers; i++) {
+/**
+* Initialize all game objects.
+*/
+public void initialize() {
+super.initialize();
 
-			a = new Point2D.Double(WINDOW_SCALE * Math.cos(i * angleIncrement),
-					WINDOW_SCALE * Math.sin(i * angleIncrement));
-			b = new Point2D.Double(WINDOW_SCALE
-					* Math.cos((i + 1) * angleIncrement), WINDOW_SCALE
-					* Math.sin((i + 1) * angleIncrement));
+//ball = new Ball(BALL_START_POSITION) ;
 
-			paddles[i] = new Paddle(a, b, PLAYER_KEYS[i][0], PLAYER_KEYS[i][1]);
+for  (int i= 0; i < nBalls; i++){
+	ball = new Ball (BALL_START_POSITION) ; 
 
-			goalLines[i] = new GoalLine(i+1, LOSING_SCORE, a, b);
+addObject(ball);
 
-			addObject(goalLines[i]);
-			addObject(paddles[i]);
+initializePlayers();
+}}
 
-			collisionEngine.addListener(paddles[i], ball,
-					new engine.events.Listener() {
 
-						@Override
-						public boolean handle(EventObject e_) {
-							CollisionEngine.CollisionEvent e = (CollisionEvent) e_;
-							
-							soundEngine.playSound("chirp.wav");
-							ball.onCollidePaddle((Paddle) e.getSource());
-							return false;
-						}
-					});
+public void start() {
+super.start();
 
-			collisionEngine.addListener(goalLines[i], ball,
-					new engine.events.Listener() {
+reset();
+}
 
-						@Override
-						public boolean handle(EventObject e_) {
-							CollisionEngine.CollisionEvent e = (CollisionEvent) e_;
-							onPlayerLose((GoalLine) e.getSource());
-							return false;
-						}
-					});
-		}
-	}
-	
-	public void reset() {
-		countdownValue = COUNTDOWN_DELAY;
-		
-		final PlethoraOfPong this_ = this;
-		
-		ActionListener timerAction = new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				countdownValue--;
-				
-				if (countdownValue == 0) {
-					countdownTimer.stop();
-					eh.dispatchEvent(new GameStartEvent(this_));
-				}
-			}
-		};
-		
-		countdownTimer = new Timer(1000, timerAction);
-		countdownTimer.start();
-	}
-	
-	public static void main(String[] args) {
-		String inputString;
-		int nPlayers = 0;
+protected void onUpdate(long tick) {}
 
-		while (nPlayers < 2) {
-			inputString = JOptionPane
-					.showInputDialog("Input the number of players:");
-	
-			try {
-				nPlayers = Integer.parseInt(inputString);
-			} catch (NumberFormatException e) {
-				return;
-			}
-		}
-		
-		new PlethoraOfPong(nPlayers).start();
-	}
-	
-	public static class GameStartEvent extends engine.events.Event {
-		
-		public GameStartEvent(Object source) {
-			super(source);
-		}
-	}
+protected void onRender(Graphics2D g) {
+
+// If the count down is in-progress display the countdown value.
+if (countdownValue > 0) {
+g.setColor(Color.GREEN);
+g.scale(1, -1);
+g.translate(-3, 3);
+g.drawString(Integer.toString(countdownValue), 0, 0);
+}
+
+if (isGameOver) {
+g.setColor(Color.RED);
+g.scale(0.15, -0.15);
+g.translate(-45, 20);
+g.drawString("Player " + loserPlayer + " Loses!", 0, 0);
+}
+}
+
+public void onPlayerLose(GoalLine goal) {	
+ball.reset();
+goal.incrementScore();
+
+if (goal.getScore() == LOSING_SCORE) {
+//soundEngine.playSound("death.wav");
+System.out.println("Player " + goal.playerNum + " loses!!");
+isGameOver = true;
+loserPlayer = goal.playerNum;
+} else {
+//soundEngine.playSound("oops.wav");
+goal.updateColor();
+reset();
+}
+}
+
+/**
+* Splits the circumference of a circle into `2*N_PLAYERS - 1` points to act
+* as the boundaries for `N_PLAYERS` player paddles, arranged in a polygon
+* with `N_PLAYERS` sides.
+*/
+private void initializePlayers() {
+
+final double angleIncrement = 2 * Math.PI / nPlayers;
+
+paddles = new Paddle[nPlayers];
+goalLines = new GoalLine[nPlayers];
+
+Point2D a, b;
+
+for (int i = 0; i < nPlayers; i++) {
+
+a = new Point2D.Double(WINDOW_SCALE * Math.cos(i * angleIncrement),
+WINDOW_SCALE * Math.sin(i * angleIncrement));
+b = new Point2D.Double(WINDOW_SCALE
+* Math.cos((i + 1) * angleIncrement), WINDOW_SCALE
+* Math.sin((i + 1) * angleIncrement));
+
+paddles[i] = new Paddle(a, b, PLAYER_KEYS[i][0], PLAYER_KEYS[i][1]);
+
+goalLines[i] = new GoalLine(i+1, LOSING_SCORE, a, b);
+
+addObject(goalLines[i]);
+addObject(paddles[i]);
+
+collisionEngine.addListener(paddles[i], ball,
+new engine.events.Listener() {
+
+@Override
+public boolean handle(EventObject e_) {
+CollisionEngine.CollisionEvent e = (CollisionEvent) e_;
+
+//soundEngine.playSound("chirp.wav");
+ball.onCollidePaddle((Paddle) e.getSource());
+return false;
+}
+});
+
+collisionEngine.addListener(goalLines[i], ball,
+new engine.events.Listener() {
+
+@Override
+public boolean handle(EventObject e_) {
+CollisionEngine.CollisionEvent e = (CollisionEvent) e_;
+onPlayerLose((GoalLine) e.getSource());
+return false;
+}
+});
+}
+}
+
+public void reset() {
+countdownValue = COUNTDOWN_DELAY;
+
+final FinalProject this_ = this;
+
+ActionListener timerAction = new ActionListener() {
+@Override
+public void actionPerformed(ActionEvent e) {
+countdownValue--;
+
+if (countdownValue == 0) {
+countdownTimer.stop();
+eh.dispatchEvent(new GameStartEvent(this_));
+}
+}
+};
+
+countdownTimer = new Timer(1000, timerAction);
+countdownTimer.start();
+}
+
+public static void main(String[] args) {
+String inputString;
+String inputBalls;
+int nPlayers = 0;
+int nBalls = 0;
+
+while (nPlayers < 2) {
+inputString = JOptionPane
+
+.showInputDialog("Input the number of players:");
+
+inputBalls = JOptionPane
+.showInputDialog("Input the number of balls:");
+try {
+nPlayers = Integer.parseInt(inputString);
+nBalls = Integer.parseInt(inputBalls);
+} catch (NumberFormatException e) {
+return;
+}
+}
+
+
+new FinalProject(nPlayers, nBalls).start();
+}
+
+public static class GameStartEvent extends engine.events.Event {
+
+public GameStartEvent(Object source) {
+super(source);
+}
 }
